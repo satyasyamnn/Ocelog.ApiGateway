20420dae72cc5d25975a56594d29a91967191a42
satyasyamnn <satyasyamnn@gmail.com>
2019-04-28 16:51:32 +0530

    First Commit

---
+++ Catalog.Api/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers
+{
+    [Route("api/v1/catalog/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private IServer _server;
+        public ValuesController(IServer server)
+        {
+            _server = server;
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Ocelot Gateway
+
+## Ocelot Big picture
+
+<https://ocelot.readthedocs.io/en/latest/introduction/bigpicture.html/>
+
+## References
+
+
+<https://ocelot.readthedocs.io/en/latest/features/routing.html/>
